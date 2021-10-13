ba2ea03e6f86ac85808c2256a4811b6f82985b10
Ling-Hao Lin <60977019+Mephisto-000@users.noreply.github.com>
2021-10-13 21:50:47 +0800

    Add 10_13 HW

---
+++ Machine_Learning/.ipynb_checkpoints/Ex3_fun-checkpoint.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sigm(x):
+    return 1.0 / (1 + np.exp(-x))
+
+def sigm_deriv(x):
+    return (1.0 / (1 + np.exp(-x))) * (1 - (1.0 / (1 + np.exp(-x))))
+
+
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_deriv(x):
+    return 1.0 - np.tan(x) * np.tan(x)
+
+
+def relu(x):
+    if x >= 0:
+        return x
+    else:
+        return 0
+
+def relu_deriv(x):
+    if x>= 0:
+        return 1
+    else:
+        return 0
+
+
+
